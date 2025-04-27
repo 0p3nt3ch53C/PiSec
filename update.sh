097ed38e7fa299ef654585229b29937f1272993f
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-04-27 18:43:11 -0400

    Add payload all the things

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y && sudo apt-get autoclean -y
 sudo apt-get clean -y
+
+# Retrieve Payload All the Things
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git PATT && cd PATT
