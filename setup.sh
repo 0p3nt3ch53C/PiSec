0ca4d8557cac9395b9bf62509c68dcae4f74bd58
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-04-28 19:38:57 -0400

    Organize and create run script

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+curl -sSL https://get.docker.com | sh
+dockerd-rootless-setuptool.sh install
+
+# Retrieve raw files for wordlists:
+# Retrieve Payload All the Things
+git clone --depth 1 https://github.com/swisskyrepo/PayloadsAllTheThings.git PATT 
+
+# Retrieve SecLists
+git clone --depth 1 https://github.com/danielmiessler/SecLists.git SL
+
+# Add fuzz DB
+git clone --depth 1 https://github.com/fuzzdb-project/fuzzdb FDB
+
+# Get fuzz.txt wordlist
+git clone --depth 1 https://github.com/Bo0oM/fuzz.txt
+
+# Get Assetnote wordlists
+# mkdir ASW && cd ASW
+# wget -r --no-parent -R "index.html*" https://wordlists-cdn.assetnote.io/data/ -nH -e robots=off
+
+# Retrieve tools
+# Retrieve XSS Hunter (docker)
+git clone --depth 1 https://github.com/mandatoryprogrammer/xsshunter-express.git Tools/XSSH 
+
+# Retrieve Katana (docker)
+git clone --depth 1 https://github.com/projecstdiscovery/katana.git Tools/KTA
+cd Tools/KTA
+DOCKER_BUILDKIT=1 docker build -t katana:latest .
+# Builds: docker.io/library/katana:latest
+# Example: docker run katana:latest -u http://scanme.nmap.org/ -system-chrome -headless > results/20250428-scanme.nmap.org.txt
+# NOTE: first time run intalls chrome.
+
+# Retrieve GoSpider (docker)
+git clone --depth 1 https://github.com/jaeles-project/gospider.git Tools/GOS
+
+# Retrieve ParamSpider (docker)
+git clone --depth 1 https://github.com/devanshbatham/ParamSpider.git Tools/PSP
+
+mkdir results && cd results
+
--- update.sh
+++ update.sh
@@ -3,19 +3,3 @@
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y && sudo apt-get autoclean -y
 sudo apt-get clean -y
-
-# Retrieve Payload All the Things
-git clone --depth 1 https://github.com/swisskyrepo/PayloadsAllTheThings.git PATT 
-
-# Retrieve SecLists
-git clone --depth 1 https://github.com/danielmiessler/SecLists.git SL
-
-# Add fuzz DB
-git clone --depth 1 https://github.com/fuzzdb-project/fuzzdb FDB
-
-# Get fuzz.txt wordlist
-git clone --depth 1 https://github.com/Bo0oM/fuzz.txt
-
-# Get Assetnote wordlists
-# mkdir ASW && cd ASW
-# wget -r --no-parent -R "index.html*" https://wordlists-cdn.assetnote.io/data/ -nH -e robots=off
