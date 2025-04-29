b0892f6c001ecd39f1a6f48600df187400f44b08
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-04-29 11:49:57 -0400

    Add gospider parameters

--- setup.sh
+++ setup.sh
@@ -28,13 +28,19 @@ git clone --depth 1 https://github.com/mandatoryprogrammer/xsshunter-express.git
 git clone --depth 1 https://github.com/projecstdiscovery/katana.git Tools/KTA
 cd Tools/KTA
 DOCKER_BUILDKIT=1 docker build -t katana:latest .
+cd ../..
 # Builds: docker.io/library/katana:latest
-# Example: docker run katana:latest -u http://scanme.nmap.org/ -system-chrome -headless | tee results/20250428-scanme.nmap.org.txt
+# Example: docker run -rm katana:latest -u http://scanme.nmap.org/ -system-chrome -headless | tee results/20250428-scanme.nmap.org.txt
+# Example: docker run -rm katana:latest -jc -d 25 -u https://rei.com/ -system-chrome -headless | tee results/20250428-rei-d25.txt &
 # NOTE: first time run intalls chrome.
 # NOTE: Creates residual containers.
 
 # Retrieve GoSpider (docker)
 git clone --depth 1 https://github.com/jaeles-project/gospider.git Tools/GOS
+cd Tools/GOS
+DOCKER_BUILDKIT=1 docker build -t gospider:latest .
+cd ../..
+# Example: docker run --rm gospider:latest -s "https://rei.com/" -v -c 10 -t 50 -d 25 --other-source --include-subs | tee results/20250429-sp-rei-d25.txt &
 
 # Retrieve ParamSpider (docker)
 git clone --depth 1 https://github.com/devanshbatham/ParamSpider.git Tools/PSP
