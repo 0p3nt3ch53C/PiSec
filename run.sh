adee0ff79e7593e62c9c5d44844e646005217342
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-05-02 17:00:36 -0400

    Add katana and related one liner updates

--- run.sh
+++ run.sh
@@ -21,10 +21,12 @@ docker run --rm cero:latest $1 | tee results/$1/$(date +%Y%m%d)-CER.txt
 # docker run --rm amass:latest enum -active -d $1 -v | tee results/$1/$(date +%Y%m%d)-AMA.txt
 
 sort -u results/$1/$(date +%Y%m%d)-*.txt > results/$1/$(date +%Y%m%d)-DOMAINS.all
+sed -i -e 's/^/https:\/\//' results/$1/$(date +%Y%m%d)-DOMAINS.all
+tr '\n' ',' < results/$1/$(date +%Y%m%d)-DOMAINS.all > results/$1/$(date +%Y%m%d)-OLDOMAINS.all
 echo "Found $(wc -l results/$1/$(date +%Y%m%d)-DOMAINS.all | awk '{print $1}') domains."
 
 echo "Attempting to spider domains found from $1..."
 
 # Spider based on all domains
 echo "Runing Katana..."
-# docker run --rm katana:latest -jc -d 25 -u https://rei.com/ -system-chrome -headless | tee results/20250428-rei-d25.txt &
+docker run --rm katana:latest -jc -d 25 -u $(cat results/$1/$(date +%Y%m%d)-OLDOMAINS.all) -system-chrome -headless | tee results/$1/$(date +%Y%m%d)-KTA.txt
