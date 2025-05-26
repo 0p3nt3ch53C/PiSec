e7feef954a38c492547c7c186c184896926abe64
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-05-26 10:14:16 -0400

    Major updates to logic target ad results directory usage

--- run.sh
+++ run.sh
@@ -54,39 +54,39 @@ echo "Continuing with: $target".
 
 # Retrieve domains
 echo "Running subfinder..."
-docker run --rm subfinder:latest -d $target -all | tee results/$1/$(date +%Y%m%d)-SBF.txt
+docker run --rm subfinder:latest -d "$target" -all | tee "$results_directory$(date +%Y%m%d)-SBF.txt"
 
 echo "Running crt..."
-docker run --rm crt:latest $1 | tee results/$1/$(date +%Y%m%d)-CRT.txt
+docker run --rm crt:latest "$target" | tee "$results_directory$(date +%Y%m%d)-CRT.txt"
 
 echo "Running cero..."
-docker run --rm cero:latest $1 | tee results/$1/$(date +%Y%m%d)-CER.txt
+docker run --rm cero:latest "$target" | tee "$results_directory$(date +%Y%m%d)-CER.txt"
 
 # WIP - Requires filtering:
 # echo "Running amass..."
 # Capture OSINT:
-# docker run --rm amass:latest intel -active -whois -d $1 -v | tee results/$1/$(date +%Y%m%d)-OSINT-AMA.txt
+# docker run --rm amass:latest intel -active -whois -d "$target" -v | tee "$results_directory$(date +%Y%m%d)-OSINT-AMA.txt"
 # Capture ENUM:
-# docker run --rm amass:latest enum -active -d $1 -v | tee results/$1/$(date +%Y%m%d)-ENUM-AMA.txt
+# docker run --rm amass:latest enum -active -d "$target" -v | tee "$results_directory$(date +%Y%m%d)-ENUM-AMA.txt"
 
-sort -u results/$1/$(date +%Y%m%d)-*.txt > results/$1/$(date +%Y%m%d)-DOMAINS.all
-sed -i -e 's/^/https:\/\//' results/$1/$(date +%Y%m%d)-DOMAINS.all
-tr '\n' ',' < results/$1/$(date +%Y%m%d)-DOMAINS.all > results/$1/$(date +%Y%m%d)-OLDOMAINS.all
-echo "Found $(wc -l results/$1/$(date +%Y%m%d)-DOMAINS.all | awk '{print $1}') domains."
+sort -u "$results_directory$(date +%Y%m%d)-*.txt" > "$results_directory$(date +%Y%m%d)-DOMAINS.all"
+sed -i -e 's/^/https:\/\//' "$results_directory$(date +%Y%m%d)-DOMAINS.all"
+tr '\n' ',' < "$results_directory$(date +%Y%m%d)-DOMAINS.all" > "$results_directory$(date +%Y%m%d)-OLDOMAINS.all"
+echo "Found $(wc -l results/$target/$(date +%Y%m%d)-DOMAINS.all | awk '{print $1}') domains."
 
-docker run -v $(pwd)/results/$1/:/app/paramspider/ --rm paramspider:latest -l /app/paramspider/$(date +%Y%m%d)-DOMAINS.all -s | tee results/$1/$(date +%Y%m%d)-PSP.txt
+docker run -v $(pwd)"$results_directory":/app/paramspider/ --rm paramspider:latest -l /app/paramspider/$(date +%Y%m%d)-DOMAINS.all -s | tee "$results_directory$(date +%Y%m%d)-PSP.txt"
 
 echo "Formatting paramspider results..."
-cat results/$1/$(date +%Y%m%d)-PSP.txt | grep -i "http" > results/$1/$(date +%Y%m%d)-PSP-FUZZ.txt
+cat "$results_directory$(date +%Y%m%d)-PSP.txt" | grep -i "http" > "$results_directory$(date +%Y%m%d)-PSP-FUZZ.txt"
 
-echo "Attempting to spider domains found from $1..."
+echo "Attempting to spider domains found from $target..."
 
 # Spider based on all domains
 echo "Runing Katana..."
-docker run --rm katana:latest -jc -d 25 -u $(cat results/$1/$(date +%Y%m%d)-OLDOMAINS.all) -system-chrome -headless | tee results/$1/$(date +%Y%m%d)-KTA.txt
+docker run --rm katana:latest -jc -d 25 -u $(cat "$results_directory$(date +%Y%m%d)-OLDOMAINS.all") -system-chrome -headless | tee "$results_directory$(date +%Y%m%d)-KTA.txt"
 
 # Print final results:
-echo "Final results from $1 include $(wc -l results/$1/$(date +%Y%m%d)-PSP-FUZZ.txt | awk '{print $1}') FUZZable URLs."
-echo "Final results from $1 include $(wc -l results/$1/$(date +%Y%m%d)-KTA.txt | awk '{print $1}') URLs."
+echo "Final results from "$target" include $(wc -l "$results_directory$(date +%Y%m%d)-PSP-FUZZ.txt" | awk '{print $1}') FUZZable URLs."
+echo "Final results from "$target" include $(wc -l "$results_directory$(date +%Y%m%d)-KTA.txt" | awk '{print $1}') URLs."
 
 set +x
